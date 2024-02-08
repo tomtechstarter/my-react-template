c03cb4eb83cb591411986800a94ce05731ce90c0
h4n5o <72876852+h4n5o@users.noreply.github.com>
2024-02-08 19:15:49 +0100

    minor adjustments

--- src/pages/Infopage/Infopage.jsx
+++ src/pages/Infopage/Infopage.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import LoadingDiv from "../../components/common/templates/loading-div";
 import StandardBtn from "../../components/common/buttons/standard-btn";
+import "./infopage.css"
 
 function InfoPage() {
   return (
     <div>
-      <LoadingDiv />
-      <StandardBtn onClick={() => alert('Hallo von der Info Seite')}>
-        Klick mich
-      </StandardBtn>
+      <div className="textPos">
+        <LoadingDiv />
+      </div>
+        <div className="btnPos">
+          <StandardBtn onClick={() => alert("Hallo von der Info Seite")}>
+            Klick mich
+          </StandardBtn>
+        </div>
     </div>
+
   );
 }
 
