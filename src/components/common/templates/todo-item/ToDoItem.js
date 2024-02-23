d8615cfc63ae7b94272042eb83295a90cd9de2c0
SirKaktor <juliuststelldinger@web.de>
2024-02-23 14:24:55 +0100

    removed deprecated comments

--- src/components/common/templates/todo-item/ToDoItem.js
+++ src/components/common/templates/todo-item/ToDoItem.js
@@ -5,7 +5,6 @@ import { TodosMutations } from "../../../../api/v1/todos";
 import StandardBtn from "../../buttons/standard-btn/StandardBtn";
 
 function ToDoItem({ todo }) {
-  // console.log("TODO", todo);
   const [isDone, setIsDone] = useState(todo.isDone);
   const [task, setTask] = useState(todo.task);
   const [dueDate, setDueDate] = useState(todo.dueDate);
@@ -13,26 +12,18 @@ function ToDoItem({ todo }) {
   const [isEditMode, setIsEditMode] = useState(false);
 
   async function onClickDone() {
-    const newTodo = await TodosMutations.markTodo(todo.id, !isDone);
+    await TodosMutations.markTodo(todo.id, !isDone);
     setIsDone(!isDone);
-    console.log("NEW TODO", newTodo);
   }
 
   async function onClickDelete() {
     setIsDeleted(true);
-    console.log("MY TDO", todo);
     await TodosMutations.deleteTodo(todo.id);
   }
 
   async function onClickSendUpdate() {
-    const response = await TodosMutations.updateTodo(
-      todo.id,
-      task,
-      isDone,
-      dueDate
-    );
+    await TodosMutations.updateTodo(todo.id, task, isDone, dueDate);
     setIsEditMode(false);
-    console.log(response);
   }
 
   async function onClickEdit() {
@@ -41,14 +32,12 @@ function ToDoItem({ todo }) {
 
   if (isDeleted) return null;
 
-  // Aktuell überschreiben wir unsere ToDo-Karte.
-  // Wenn du heute die andere Ansicht haben willst, musst du
-  // den folgenden Block auskommentieren.
   if (isEditMode) {
     return (
       <div className={styles.mainContainer}>
         <h1> EDIT </h1>
         <div className={styles.horizontalLine}></div>
+
         <input
           type="text"
           value={task}
@@ -63,7 +52,6 @@ function ToDoItem({ todo }) {
           isChecked={isDone}
           onClick={() => setIsDone(!isDone)}
         ></Checkbox>
-
         <StandardBtn text={"SEND"} onClick={onClickSendUpdate} />
       </div>
     );
