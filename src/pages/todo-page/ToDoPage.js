222544f46c8520d3828721895934b99dcc164815
tomtechstarter <149696023+tomtechstarter@users.noreply.github.com>
2024-02-28 12:50:01 +0100

    Divide TodoPage into AddTodo and TodoPage

--- src/pages/todo-page/ToDoPage.js
+++ src/pages/todo-page/ToDoPage.js
@@ -1,3 +1,4 @@
+import AddTodo from "./add-todo";
 import { TodosMutations, TodosQueries } from "../../api/v1/todos";
 import Checkbox from "../../components/common/buttons/checkbox";
 import StandardBtn from "../../components/common/buttons/standard-btn/StandardBtn";
@@ -11,10 +12,6 @@ function ToDoPage() {
   //neurendern der Komponente triggern
   //Antwort ist da? => lad die Komponente einmal mit Daten gefüllt neu
   const [todos, setTodos] = useState([]);
-  const [newUserId, setNewUserId] = useState("");
-  const [newTask, setNewTask] = useState("");
-  const [newDueDate, setNewDueDate] = useState("");
-  const [newIsDone, setNewIsDone] = useState(false);
 
   async function fetchTodos() {
     try {
@@ -27,18 +24,6 @@ function ToDoPage() {
     } catch (e) {
       console.log("Hello world", e);
     }
-  }
-
-  async function onClickAdd() {
-    console.log("Ich poste jetzt!");
-    const newTodo = await TodosMutations.createTodo(
-      newUserId,
-      newTask,
-      newIsDone,
-      newDueDate
-    );
-
-    setTodos([...todos, newTodo]);
   }
 
   // Alternative Funktion für den API Aufruf
@@ -68,41 +53,14 @@ function ToDoPage() {
 
   //###Ergänzung zum Code vom Unterricht:###
   //Wenn todos noch leer ist, gib leeren Container aus
-  if (todos.length == 0) {
-    return <div className={styles.mainContainer}></div>;
-  }
+  if (todos.length === 0) return null;
 
   //wenn wir die Daten in todos drin haben (durch den fetch)
   //können wir auch todos, sowie dessen Inhalte in Probs weitergeben
   //hier: "todo={todos[1]}" gibt das 2. todo weiter.
   return (
     <div className={styles.mainContainer}>
-      <div>
-        <input
-          type="text"
-          value={newUserId}
-          placeholder="New User ID..."
-          onChange={(event) => setNewUserId(event.target.value)}
-        ></input>
-        <input
-          type="text"
-          value={newTask}
-          placeholder="aufgabe..."
-          onChange={(event) => setNewTask(event.target.value)}
-        ></input>
-        <input
-          type="text"
-          value={newDueDate}
-          placeholder="datum..."
-          onChange={(event) => setNewDueDate(event.target.value)}
-        ></input>
-        <Checkbox
-          isChecked={newIsDone}
-          onClick={() => setNewIsDone(!newIsDone)}
-        ></Checkbox>
-        <StandardBtn text={"ADD"} onClick={onClickAdd} />
-      </div>
-
+      <AddTodo setTodos={setTodos} />
       <div>
         {todos.map((item) => (
           <ToDoItem key={item.id} todo={item} />
